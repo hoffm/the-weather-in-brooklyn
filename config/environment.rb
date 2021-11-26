b049ddc24f63b44630896997c34e312d143020f0
Michael Hoffman <michael.s.hoffman@gmail.com>
2021-11-25 22:26:53 -0500

    Make data dirs on boot

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require "aws-sdk-polly"
 require "aws-sdk-s3"
 require "dotenv"
 require "faraday"
+require "fileutils"
 require "json"
 require "nokogiri"
 
@@ -11,4 +12,7 @@ Dotenv.load('.env', '/etc/secrets/.render_env')
 TWIB_APP_PATH = Dir.pwd + "/lib/twib"
 require TWIB_APP_PATH + "/constants.rb"
 Dir["#{TWIB_APP_PATH}/**/*.rb"].each { |file| require file }
+FileUtils.mkdir_p(Twib::JSON_DIR)
+FileUtils.mkdir_p(Twib::RSS_DIR)
+
 Time.zone = "America/New_York"
